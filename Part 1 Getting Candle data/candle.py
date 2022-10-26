eb6ad5fd8b398f9059dfc914fe883c027883a0a3
Shreehari A <dragonzurfer@gmail.com>
2022-10-26 10:20:44 +0530

    Correct data json

--- Part 1 Getting Candle data/candle.py
+++ Part 1 Getting Candle data/candle.py
@@ -14,13 +14,12 @@ client_id = apiCredJson["ClientID"]
 fyers = fyersModel.FyersModel(client_id=client_id, token=access_token,log_path="/Users/drago/Documents/ytFyersAPI")
 
 data = {
-    "symbol":"NSE:FINNIFTY-INDEX",
-    "resolution":"D",
-    "date_format":"0",
-    "range_from":"1666441392",
-    "range_to":"1666441392",
-    "cont_flag":"1"
-    }
-
+            "symbol":"NSE:NIFTY50-INDEX",
+                "resolution":"D",
+                    "date_format":"0",
+                        "range_from":"1666310400",
+                            "range_to":"1666441392",
+                                "cont_flag":"1"
+                                    }
 response = fyers.history(data)
 print(response)
